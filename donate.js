aaaa986815b05da1bcaa32cc8190c18f68b880fc
Dave Benvenuti <davebenvenuti@gmail.com>
2017-06-04 09:49:55 -0700

    Support for donations

---
+++ donate.js
@@ -0,0 +1,127 @@
+"use strict";
+
+var Stripe = require('stripe');
+var _ = require('lodash');
+
+
+var Express = require('express');
+
+if(process.env.NODE_ENV != 'test') {
+    var Webtask = require('webtask-tools');
+}
+
+var app = Express();
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded())
+
+const REQUIRED = [
+    'firstname',
+    'lastname',
+    'address1',
+    'city',
+    'state',
+    'zip',
+    'amount',
+    'email',
+    'stripeToken'
+]
+
+var attributeErrors = function(attributes) {
+    return _.compact(_.map(_.pairs(attributes), function(keyValuePair) {
+        var attr = keyValuePair[0];
+        var value = keyValuePair[1];
+
+        if(!_.contains(REQUIRED, attr)) { return; }
+
+        if(!value || !value.length) {
+            return attr + " can't be blank";
+        } else {
+            return null;
+        }
+    }));
+};
+
+var donate = function(req, res) {
+    var ctx = req.webtaskContext;
+
+    var attributes = {};
+
+    [
+        'firstname',
+        'lastname',
+        'address1',
+        'address2',
+        'city',
+        'state',
+        'zip',
+        'phone',
+        'amount',
+        'email',
+        'stripeToken'
+    ].forEach(function(attribute) {
+        attributes[attribute] = req.body[attribute];
+    });
+
+    var errorMessages = attributeErrors(attributes);
+
+    if(errorMessages.length) {
+        res.status(400).send(errorMessages.join("\n"));
+
+        return;
+    }
+
+    var customerAttributes = {};
+
+    var name = attributes.firstname + " " + attributes.lastname;
+
+    customerAttributes.description = name;
+    customerAttributes.email = attributes.email;
+    customerAttributes.source = attributes.stripeToken;
+    customerAttributes.shipping = {
+        name: name,
+        address: {
+            line1: attributes.address1,
+            line2: attributes.address2,
+            city: attributes.city,
+            state: attributes.state,
+            postal_code: attributes.zip
+        },
+        phone: attributes.phone
+    };
+
+    customerAttributes.metadata = {
+        donation: true
+    };
+
+    var stripe = Stripe(ctx.secrets.stripeSecretKey);
+
+    return stripe.customers.create(customerAttributes)
+        .then(function(customer) {
+            return stripe.charges.create({
+                amount: attributes.amount,
+                currency: 'usd',
+                customer: customer.id,
+                description: attributes.firstname + " " + attributes.lastname,
+                metadata: {
+                    donation: true
+                }
+            });
+        })
+        .then(function(charge) {
+            res.redirect(ctx.secrets.successRedirect);
+
+            return charge;
+        })
+        .catch(function(error) {
+            res.status(400).text(error.message);
+        });
+};
+
+app.post('/donate', donate);
+
+if(process.env.NODE_ENV != 'test') {
+    module.exports = Webtask.fromExpress(app);
+}
+
+module.exports.donate = donate;
---
+++ test/donate_test.js
@@ -0,0 +1,159 @@
+var testHelper = require('./test_helper');
+var stripe = testHelper.stripe;
+
+var donate = require('../donate').donate;
+var expect = require('expect.js');
+var _ = require('lodash');
+var util = require('../util');
+var Promise = require('bluebird');
+
+describe("donate", function() {
+    this.timeout(30000);
+
+    beforeEach(function() {
+        this.req = {
+            body: {},
+            webtaskContext: {
+                secrets: {
+                    stripeSecretKey: process.env['STRIPE_SECRET_KEY'],
+                    successRedirect: "https://seattledsa.org"
+                }
+            }
+        };
+
+        this.res = {
+            redirected: false,
+            status: function(code) {
+                this.lastStatus = code;
+                return this;
+            },
+            send: function(text) {
+                this.lastText = text;
+                return this;
+            },
+            redirect: function(url) {
+                this.redirected = true;
+                this.lastRedirect = url;
+            }
+        };
+    });
+
+    // TODO use replay or something similar so we don't have to do this
+    after(testHelper.cleanupStripe.bind(testHelper));
+
+    describe("on success", function() {
+        beforeEach(function(done) {
+            this.ranAt = (new Date()).valueOf();
+
+            this.req.body.firstname = "Rosa";
+            this.req.body.lastname = "Luxemburg " + this.ranAt.toString();
+            this.req.body.address1 = "123 Main St";
+            this.req.body.address2 = "Apt 7";
+            this.req.body.city = "Seattle";
+            this.req.body.state = "WA";
+            this.req.body.zip = "98102";
+            this.req.body.phone = "867-5309";
+            this.req.body.amount = "7700";
+            this.req.body.email = "rosa.l@fake.email";
+
+            var _req = this.req;
+
+            util.createStripeToken().then(function(stripeToken) {
+                _req.body.stripeToken = stripeToken.id;
+
+                done();
+            });
+        });
+
+        it("creates a stripe customer", function(done) {
+            var _ranAt = this.ranAt;
+
+            donate(this.req, this.res)
+                .then(function() {
+                    return stripe.customers.list();
+                })
+                .then(function(customers) {
+                    var name = "Rosa Luxemburg " + _ranAt;
+
+                    var newCustomer = _.find(customers.data, { description: name });
+
+                    expect(newCustomer).to.be.ok();
+                    expect(newCustomer.description).to.be(name);
+                    expect(newCustomer.shipping).to.be.ok();
+                    expect(newCustomer.shipping.address).to.be.ok();
+                    expect(newCustomer.shipping.address.line1).to.be("123 Main St");
+                    expect(newCustomer.shipping.address.line2).to.be("Apt 7");
+                    expect(newCustomer.shipping.address.city).to.be("Seattle");
+                    expect(newCustomer.shipping.address.state).to.be("WA");
+                    expect(newCustomer.shipping.address.postal_code).to.be("98102");
+                    expect(newCustomer.shipping.phone).to.be("867-5309");
+                    expect(newCustomer.sources.data).to.be.ok();
+                    expect(newCustomer.sources.data.length).to.be(1);
+                    expect(newCustomer.sources.data[0].object).to.be("card");
+                    expect(newCustomer.metadata.donation).to.be("true");
+                    expect(newCustomer.metadata.nationalDonate).to.be(undefined);
+
+                    done();
+                })
+                .catch(done);
+        });
+
+        it("creates a stripe charge", function(done) {
+            var _ranAt = this.ranAt;
+
+            donate(this.req, this.res)
+                .then(function() {
+                    return stripe.charges.list();
+                })
+                .then(function(charges) {
+                    var newCharge = _.find(_.filter(charges.data, { amount: 7700 }), function(charge) {
+                        return charge.created >= _ranAt / 1000;
+                    });
+
+                    expect(newCharge).to.be.ok();
+                    expect(newCharge.metadata.donation).to.be("true");
+
+                    done();
+                })
+                .catch(done);
+        });
+
+        it("redirects to configured url", function(done) {
+            var _res = this.res;
+            donate(this.req, _res)
+                .then(function() {
+                    expect(_res.redirected).to.eql(true);
+                    expect(_res.lastRedirect).to.be("https://seattledsa.org");
+                    done();
+                })
+                .catch(done);
+        });
+    });
+
+    describe("with missing parameters", function() {
+        // These tests don't need to be asynchronous since a promise doesn't get
+        // returned if validation fails
+        beforeEach(function() {
+            this.ranAt = (new Date()).valueOf();
+
+            this.req.body.firstname = "Rosa";
+            this.req.body.lastname = "Luxemburg " + this.ranAt.toString();
+            this.req.body.address1 = "123 Main St";
+            this.req.body.address2 = "Apt 7";
+            this.req.body.city = "Seattle";
+            this.req.body.state = "WA";
+            this.req.body.zip = "98102";
+            this.req.body.phone = "867-5309";
+            this.req.body.amount = "7700";
+
+            // Missing stripeToken and email
+        });
+
+        it("returns a 400 with error messages", function() {
+            donate(this.req, this.res);
+            expect(this.res.lastStatus).to.eql(400);
+            expect(this.res.lastText).to.match(/stripeToken can't be blank/);
+            expect(this.res.lastText).to.match(/email can't be blank/);
+        });
+    });
+});
